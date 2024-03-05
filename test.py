dcdb70ab35177ebf67a2f0391c0924230a1323a6
Youssef Abdelhamid <108409231+ClassicEntry@users.noreply.github.com>
2024-03-06 02:39:15 +1100

    test

--- test.py
+++ test.py
@@ -1,5 +1,25 @@
 print("This is a test")
 print("Attention this is a test!!!!!!!!!!!!!!!!")
 print("Version Control Test 0.1")
-#IT project 1
-#Bit and Bytes or Bits N bytes
+# IT project 1
+# Bit and Bytes or Bits N bytes
+print("H")
+
+
+def primes(n: int):
+    """Return a list of the first n primes."""
+    sieve = [True] * n
+
+    res = []
+
+    for i in range(2, n):
+        if sieve[i]:
+            res.append(i)
+            for j in range(i * i, n, i):
+                sieve[j] = False
+
+    return res
+
+
+xs = primes(100)
+print(xs)
